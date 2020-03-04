a732815a947d0d03817ea8a7c478848f522966c0
Bioinfoplanet <Bioplanet520@outlook.com>
2020-03-04 18:16:25 +0800

    first upload

---
+++ R/foo.R
@@ -0,0 +1,6 @@
+##' Basic arithmetic
+##'
+##' @param x,y numeric vectors.
+add <- function(x, y) x + y
+##' @rdname add
+times <- function(x, y) x * y
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(bioinfor)
+
+test_check("bioinfor")
